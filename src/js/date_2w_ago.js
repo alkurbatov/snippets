b554b51a04a88a27b213cc6b24b472887972b8b6
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-03-16 11:41:02 +0300

    refacor(js): Make the example a bit more verbose
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- src/js/date_2w_ago.js
+++ src/js/date_2w_ago.js
@@ -1,5 +1,6 @@
+console.log(`Now: ${new Date()}`)
+
 // NOTE (alkurbatov): 12096e5 is a magic number,
 // represents count of milliseconds in two weeks.
 const past = new Date(Date.now() - 12096e5)
-
-console.log(past.toString())
+console.log(`Now - 2w: ${past.toString()}`)
