c3c40f46d140dee88f94f400853ba70887e17231
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-05-29 13:51:44 +0300

    style(python): Make linter happier
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- src/python/pycharm_remote_debug.py
+++ src/python/pycharm_remote_debug.py
@@ -1,5 +1,5 @@
-import os
 import pydevd
 
-pydevd.settrace('172.16.45.157', port=9090, stdoutToServer=True, stderrToServer=True, suspend=False)
+pydevd.settrace('172.16.45.157', port=9090,
+                stdoutToServer=True, stderrToServer=True, suspend=False)
 print('Hello world!')
