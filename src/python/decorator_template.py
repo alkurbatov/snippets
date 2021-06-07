0eba4bd4852dd34822610f91ef7f354fbf8c3526
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-06-07 16:07:40 +0300

    feat(python): Add decorator template
    Inspired by examples at Real Python.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

---
+++ src/python/decorator_template.py
@@ -0,0 +1,14 @@
+'''
+Kudos to https://realpython.com/primer-on-python-decorators/
+'''
+
+import functools
+
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator
