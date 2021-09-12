fdfff689da72192917084f0ce9231dd65a668456
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2021-09-12 14:33:30 +0300

    feat(python): Add 'measure execution time' snippet
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

---
+++ src/python/measure_execution_time.py
@@ -0,0 +1,11 @@
+import datetime
+import time
+
+import humanize
+
+started_at = time.perf_counter()
+
+time.sleep(5)
+
+elapsed = datetime.timedelta(seconds=(time.perf_counter() - started_at))
+print(humanize.naturaldelta(elapsed))
