a0ede05e2acd63bfe9c8572a3200e317c6d41ad9
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2022-01-30 14:47:03 +0300

    feat(python): Search for a space in string with timings
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

---
+++ src/python/search_spaces.py
@@ -0,0 +1,40 @@
+"""Different ways of determing whether a string contains spaces
+with performance measures."""
+
+import re
+import timeit
+
+TEXT = "this is a long long long long long string"
+SPACE_REGEX = re.compile(r"\s")
+
+linear_search_using_in = """
+' ' in TEXT
+"""
+
+linear_search_using_isspace = """
+for char in TEXT:
+    if char.isspace():
+        break
+"""
+
+linear_search_using_regexp = """
+re.search(r'\s', TEXT)
+"""
+
+linear_search_using_compiled_regexp = """
+re.search(SPACE_REGEX, TEXT)
+"""
+
+timing = timeit.timeit(linear_search_using_in, globals=globals(), number=100000)
+print(f"Linear search using in: {timing}")
+
+timing = timeit.timeit(linear_search_using_isspace, globals=globals(), number=100000)
+print(f"Linear search using isspace: {timing}")
+
+timing = timeit.timeit(linear_search_using_regexp, globals=globals(), number=100000)
+print(f"Linear search using regexp: {timing}")
+
+timing = timeit.timeit(
+    linear_search_using_compiled_regexp, globals=globals(), number=100000
+)
+print(f"Linear search using compiled regexp: {timing}")
