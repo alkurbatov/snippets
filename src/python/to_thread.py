7ee2cd54c475fc9d405c8b7f08eb2c94f2db6733
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2022-05-12 12:46:03 +0300

    feat(python): Add implementation of asyncio.to_thread
    Frequently needed for code in Python < 3.9.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

---
+++ src/python/to_thread.py
@@ -0,0 +1,17 @@
+import asyncio
+import contextvars
+import functools
+from typing import Any, Callable
+
+
+async def to_thread(func: Callable, /, *args, **kwargs) -> Any:
+    """Run blocking code in a separate thread with possibility to asynchroniously wait
+    for result.
+    See: https://github.com/python/cpython/blob/main/Lib/asyncio/threads.py
+
+    (!) Should be used on Python < 3.9, in other case please prefer asyncio.to_thread.
+    """
+    loop = asyncio.get_running_loop()
+    ctx = contextvars.copy_context()
+    func_call: Callable = functools.partial(ctx.run, func, *args, **kwargs)
+    return await loop.run_in_executor(None, func_call)
