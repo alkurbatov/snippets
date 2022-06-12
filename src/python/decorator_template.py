adf7618faf2d64c8581ac0f96bceb8434fc17799
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2022-06-12 13:07:10 +0300

    feat: Add more decorators examples
    Also, add typings.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- src/python/decorator_template.py
+++ src/python/decorator_template.py
@@ -1,14 +1,66 @@
-'''
+"""
 Kudos to https://realpython.com/primer-on-python-decorators/
-'''
+"""
 
 import functools
+from typing import Any, Awaitable, Callable, TypeVar
 
-def decorator(func):
+T = TypeVar("T")
+
+
+def decorator(func: Callable[..., Callable[T]]) -> Callable[..., Callable[T]]:
     @functools.wraps(func)
-    def wrapper_decorator(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> T:
         # Do something before
         value = func(*args, **kwargs)
         # Do something after
         return value
-    return wrapper_decorator
+
+    return wrapper
+
+
+def decorator_with_args(_some_arg: Any) -> Callable:
+    # Do something with _some_arg.
+
+    def actual_decorator(
+        func: Callable[..., Callable[T]]
+    ) -> Callable[..., Callable[T]]:
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs) -> T:
+            # Do something before
+            value = func(*args, **kwargs)
+            # Do something after
+            return value
+
+        return wrapper
+
+    return actual_decorator
+
+
+def async_decorator(func: Callable[..., Awaitable[T]]) -> Callable[..., Awaitable[T]]:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs) -> T:
+        # Do something before
+        value = await func(*args, **kwargs)
+        # Do something after
+        return value
+
+    return wrapper
+
+
+def async_decorator_with_args(_some_arg: Any) -> Callable:
+    # Do something with _some_arg.
+
+    def actual_decorator(
+        func: Callable[..., Awaitable[T]]
+    ) -> Callable[..., Awaitable[T]]:
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs) -> T:
+            # Do something before
+            value = await func(*args, **kwargs)
+            # Do something after
+            return value
+
+        return wrapper
+
+    return actual_decorator
