40699389ee840eb02b703f0d8a4923f7b5c692d5
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2023-03-25 16:28:32 +0300

    👷 Use Python 3.11 syntax
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- python/src/list_init.py
+++ python/src/list_init.py
@@ -1,9 +1,7 @@
-from typing import List, Optional
-
 # Init a list with X empty values:
-new_list: List[Optional[int]] = [None] * 5
+new_list: list[int | None] = [None] * 5
 print(new_list)
 
 # Init a list with X empty lists:
-new_list_of_lists: List[List[int]] = [[] for _ in range(5)]
+new_list_of_lists: list[list[int]] = [[] for _ in range(5)]
 print(new_list_of_lists)
