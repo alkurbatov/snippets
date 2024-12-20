0055cc2649dd3cec834fad4e0e4885d5aded2bd9
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2024-12-20 21:02:56 +0300

    👷 (golang) Improve random string example
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- golang/randomstr.go
+++ golang/randomstr.go
@@ -2,21 +2,37 @@ package snippets
 
 import (
 	"crypto/rand"
-	"crypto/sha256"
-	"encoding/hex"
+	"fmt"
 )
 
 // entropySize count of bytes for generation of random symbol sequences.
-const entropySize = 24
+// The length is taken from recommendations in
+// // https://pkg.go.dev/golang.org/x/crypto/sha3#hdr-Recommendations
+const entropySize = 32
 
-// GenerateString generates secure random string that can be used e.g. as state or nonce.
-func GenerateString() (string, error) {
-	data := make([]byte, entropySize)
-	if _, err := rand.Read(data); err != nil {
-		return "", err
+const base32alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+
+var SecureRandString = NewRandString(entropySize, base32alphabet)
+
+// NewRandString generates secure random string that can be used e.g. as state or nonce.
+// In Golang 1.24+ use https://pkg.go.dev/crypto/rand@master#Text
+// NewRandString генерирует случайную строку.
+func NewRandString(length uint, alphabet string) string {
+	if length == 0 {
+		panic("zero length random string requested")
 	}
 
-	sum := sha256.Sum256(data)
+	l := len(alphabet)
 
-	return hex.EncodeToString(sum[:]), nil
+	// NB (alkurbatov): Based on https://pkg.go.dev/crypto/rand@master#Text
+	src := make([]byte, length)
+	if _, err := rand.Read(src); err != nil {
+		panic(fmt.Errorf("read random bytes: %w", err))
+	}
+
+	for i := range src {
+		src[i] = alphabet[int(src[i])%l]
+	}
+
+	return string(src)
 }
