29d0cc9811ab434fe7e698034cc336462f36c5ea
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2025-02-17 23:05:49 +0300

    🔧 (golang) Add suppress slog example for Go 1.24
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- golang/silenceslog.go
+++ golang/silenceslog.go
@@ -5,11 +5,18 @@ import (
 	"log/slog"
 )
 
-// SuppressSlog demonstrates the way of disabling slog output completely
-// e.g. in case of tests and benchmarks.
-func SuppressSlog() {
+// SuppressSlogBeforeGo124 demonstrates the Go <= 1.23 way of disabling slog output
+// completely e.g. in case of tests and benchmarks.
+func SuppressSlogBeforeGo124() {
 	log := slog.New(
 		slog.NewTextHandler(io.Discard, nil),
 	)
+	log.Info("Prints nothing")
+}
+
+// SuppressSlog demonstrates the way of disabling slog output completely
+// e.g. in case of tests and benchmarks.
+func SuppressSlog() {
+	log := slog.New(slog.DiscardHandler)
 	log.Info("Prints nothing")
 }
